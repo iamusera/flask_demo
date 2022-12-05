a79ed8c593bf0514a6fac69ca0a4feab85c5a71c
iamusera <99506298@qq.com>
2022-12-05 12:20:39 +0800

    异常配置

--- application/middleware/exception_middleware.py
+++ application/middleware/exception_middleware.py
@@ -9,9 +9,9 @@ logger = logging.getLogger('flask_log')
 def global_exception_handler(app):
     @app.errorhandler(Exception)
     def framework_error(e):
-        """ 全局返回json类型异常 """
+        """ 全局返回json类型异常，根据异常自己配 """
         if isinstance(e, ApiException):
-            return e
+            return ServerError(msg=str(e))
         if isinstance(e, HTTPException):
             logger.error(e, exc_info=True)
             code = e.code
@@ -20,7 +20,7 @@ def global_exception_handler(app):
         else:
             if current_app.config["DEBUG"]:
                 logger.error(e, exc_info=True)
-                return e
+                return ServerError(msg=str(e))
             else:
                 logger.error(e, exc_info=True)
                 return ServerError(msg=str(e))
